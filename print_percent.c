53eef7ab5945ccf5fdb15b558f4e75c997260142
Ayanda16 <ntselea16@gmail.com>
2023-01-23 04:23:10 -0800

    format specifiers

---
+++ print_percent.c
@@ -0,0 +1,12 @@
+#include"main.h"
+
+/**
+ * print_percent - Prints a percent symbol
+ * @args: argument lists
+ * Return: Will return the amount of characters printed.
+ */
+int print_percent(__attribute__((unused))va_list args)
+{
+	_putchar('%');
+	return (1);
+}
