1a23ec546e496524986ccd5cdfd9576570c48a7a
Ayanda16 <ntselea16@gmail.com>
2023-01-24 13:22:11 -0800

    number 10

---
+++ write_buffer.c
@@ -0,0 +1,28 @@
+#include"main.h"
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ * Description: using a local buffer of 1024 to call write
+ * as little as possible
+ */
+
+int _putchar(char c)
+{
+	static char buffer[1024];
+	static int i;
+
+	if (c == -1 || i >= 1024)
+	{
+		write(1, &buffer, i);
+		i = 0;
+	}
+	if (c != -1)
+	{
+		buffer[i] = c;
+		i++;
+	}
+	return (1);
+}
